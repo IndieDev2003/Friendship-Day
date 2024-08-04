d4a5560238e78e4e99a33cd92eb123d31476d8a2
Gagan Suman <108719011+IndieDev2003@users.noreply.github.com>
2024-08-04 09:00:15 +0000

    Much

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 var config = {
     lines: [
         "Dear Aashi,",
-        "The Day When We Started Talking I Never Knew That You Will Mean To So Much",
+        "The Day When We Started Talking I Never Knew That You Will Mean So Much TO Me",
         "On That I Day Never Knew That We Fall In Love With Each other",
         "On That Day I Never Knew We Are We Will Be In Relationship ",
         "Thanks For Coming In my Life",
