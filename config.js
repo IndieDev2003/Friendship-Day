5e1506ff4b67ae63e6ad0fd7c12ac11fc2c75d43
Gagan Suman <108719011+IndieDev2003@users.noreply.github.com>
2024-08-04 08:09:03 +0000

    h

--- config.js
+++ config.js
@@ -3,8 +3,7 @@ var config = {
     lines: [
         "Dear Aashi,",
         "The Day When We Started Talking I Never Knew That You Will Mean To So Much",
-        "On That I Day Never Knew That We Fall In LOve With Each other,
-        "
+        "On That I Day Never Knew That We Fall In LOve With Each other",
        
     ],
     date:"2023-07-07",
